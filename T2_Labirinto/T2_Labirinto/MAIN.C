235875bdee51346776dba78df09756f23c97fc80
Pedro <pedrohddb@gmail.com>
2019-11-01 16:27:10 -0300

    Main.c adaptada para as mudancas q o Alexandre fez

--- T2_Labirinto/T2_Labirinto/MAIN.C
+++ T2_Labirinto/T2_Labirinto/MAIN.C
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "LABIRINTO.H"
 #include "TST_ESPC.H"
 #include <stdio.h>
@@ -6,10 +5,10 @@
 
 int main(void) {
 	
-	char comando, parametro; //inicializa a variável comando para entrar no while
+	char comando=0, parametro; //inicializa a variável comando para entrar no while
 	int tam = 3;
 	LAB_tpDirecao dir; //variavel pra andar
-	LAB_tpCondRet CondRetObtido; //variavel que recebe o condret da funcao (ainda nao e utilizada)
+	//LAB_tpCondRet CondRetObtido; //variavel que recebe o condret da funcao (ainda nao e utilizada)
 	LAB_tpElemCasa parametroENUM; //variavel que converte o caractere digitado para o o tipo de elemento que sera colocado na casa
 	LAB_tppLabirinto labTeste = NULL; // iniciação da variável
 
@@ -74,7 +73,7 @@ int main(void) {
 			LAB_ImprimirLabirinto( labTeste );
 
 		else if (comando == 'r')
-			LAB_ExisteSolucao( labTeste ); //N sei se essa funcao vai ser a q acha a solucao, mas e a mais proxima do q a gnt tem, entao botei como placeholder
+			LAB_ExibeSolucao( labTeste ); //N sei se essa funcao vai ser a q acha a solucao, mas e a mais proxima do q a gnt tem, entao botei como placeholder
 
 		else if (comando == 't')
 			printf("\n'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO:\n-'p', para inserir uma parede\n-'v', para uma casa vazia\n-'e', para setar a entrada da matriz\n-'s', para setar a saida da matriz\n\n'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO\n-'n', para andar na direcao norte\n-'l', para andar na direcao leste\n-'s', para andar na direcao sul\n-'o', para andar na direcao oeste\n\n'd' PARA DESTRUIR O LABIRINTO\n'm' PARA MOSTRAR O LABIRINTO\n'r' PARA RESOLVER O LABIRINTO\n't' PARA REVER O TUTORIAL\n'q' PARA FECHAR O PROGRAMA, MOSTRANDO SER FRACO E NAO TER A CAPACIDADE DE MANUSEAR UM LABIRINTO\n\n");
@@ -86,63 +85,4 @@ int main(void) {
 	}
 
 	return 0;
-=======
-#include "LABIRINTO.H"
-#include "TST_ESPC.H"
-#include <stdio.h>
-#include <ctype.h>
-
-int main(void) {
-	
-	char comando = 0, parametro; //inicializa a variável comando para entrar no while
-	int tam = 3;
-	LAB_tpElemCasa parametroENUM; //variavel que converte o caractere digitado para o o tipo de elemento que sera colocado na casa
-	LAB_tppLabirinto labTeste = NULL; // iniciação da variável
-
-	printf("========================= CRIACAO DO LABIRINTO =========================\n\n");
-	printf("Primeiramente, escolha o tamanho que sera utilizado na criacao do labirinto: ");                  /* TUTORIAL ESTÁ EM WIP */
-	scanf("%d", &tam);
-	LAB_CriarLabirinto(&labTeste, tam); // criação do labirinto
-
-	printf("\n\n======================= MANIPULACAO DO LABIRINTO =======================\n");
-	printf("\n\nOS SEGUINTES COMANDOS ESTAO DISPONIBILIZADOS PARA MANIPULACAO DO LABIRINTO: \n\n");
-
-	printf("'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO:\n-'p', para inserir uma parede\n-'v', para uma casa vazia\n-'e', para setar a entrada da matriz\n's', para setar a sa'ida da matriz\n\n'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO\n'd' PARA DESTRUIR O LABIRINTO\n'm' PARA MOSTRAR O LABIRINTO\n'r' PARA RESOLVER O LABIRINTO\n'q' PARA FECHAR O PROGRAMA, MOSTRANDO SER FRACO E NÃO TER A CAPACIDADE DE MANUSEAR UM LABIRINTO\n\n");
-		
-	while(comando != 'q') {
-		printf("Digite o comando desejado seguido do parametro, caso necessario: ");
-		scanf("%c", &comando);
-		if (comando == 'i') { //inserir elemento
-			scanf(" %c", &parametro);
-			if (parametro == 'p' || parametro == 'v' || parametro == 'e' || parametro == 's'){
-				if(parametro == 'p')
-					parametroENUM = LAB_ElemParede;
-				else if(parametro == 'v')
-					parametroENUM = LAB_ElemVazio;
-				else if(parametro == 'e')
-					parametroENUM = LAB_ElemEntrada;
-				else if(parametro == 's')
-					parametroENUM = LAB_ElemSaida;
-				LAB_InserirElemento(labTeste, parametroENUM);
-			}
-			else
-				printf("Parametro invalido!\n\n");
-			
-		}
-		/*else if (comando == 'a')
-
-		else if (comando == 'd')
-
-		else if (comando == 'm')
-
-		else if(comando == 'r')
-
-		else
-			printf("COMANDO INVALIDO");*/
-
-
-	}
-
-	return 0;
->>>>>>> a631ef608b67765e08caf0c31dc3c53bac7b8ae0
 }
