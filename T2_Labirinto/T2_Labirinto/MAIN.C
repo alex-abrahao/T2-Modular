4b8fc21c12b7d44e79dd5b2f0f34a7c76cdb08f2
Pedro <pedrohddb@gmail.com>
2019-11-03 15:14:44 -0300

    Main age de acordo com os condrets e foi documentada

--- T2_Labirinto/T2_Labirinto/MAIN.C
+++ T2_Labirinto/T2_Labirinto/MAIN.C
@@ -1,3 +1,27 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo labirinto
+*
+*  Arquivo gerado:              MAIN.C
+*  Letras identificadoras:      MLAB
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: Disciplinas INF 1628 / 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: aaf - Alexandre Abrahão Ferreira
+*           jmb - João Marcello Bessa
+*           phd - Pedro Henrique Dias
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*       1.00   jmb   30/10/2019 Início do desenvolvimento
+*       1.10   phd   31/10/2019 Todas funções estao disponiveis
+*       1.20   jmb   02/11/2019 Diversas otimizacoes
+*		1.30   phd   03/11/2019 Respostas adequadas aos CondRets recebidos
+*
+***************************************************************************/
+
 #include "LABIRINTO.H"
 #include "TST_ESPC.H"
 #include <stdio.h>
@@ -5,37 +29,38 @@
 
 int main(void) {
 	
-	char comando=0, parametro, marcador = 250; //inicializa a variável comando para entrar no while
-	int tam = 3;
-	LAB_tpDirecao dir; //variavel pra andar
-	//LAB_tpCondRet CondRetObtido; //variavel que recebe o condret da funcao (ainda nao e utilizada)
-	LAB_tpElemCasa parametroENUM; //variavel que converte o caractere digitado para o o tipo de elemento que sera colocado na casa
-	LAB_tppLabirinto labTeste = NULL; // iniciação da variável
+	char comando=0, parametro; // inicializa as variáveis comando e parametro para entrar no while
+	int tam = 3, x, y; // inicializa as variáveis de tamanho e das coordenadas x e y
+	LAB_tpDirecao dir; // variavel pra andar
+	LAB_tpCondRet CondRetObtido; // variavel que recebe o condret da funcao
+	LAB_tpElemCasa parametroENUM; // variavel que converte o caractere digitado para o o tipo de elemento que sera colocado na casa
+	LAB_tppLabirinto labTeste = NULL; // iniciação do labirinto
 
 	printf("========================= CRIACAO DO LABIRINTO =========================\n\n");
-	printf("Primeiramente, escolha o tamanho que sera utilizado na criacao do labirinto: ");                  /* TUTORIAL ESTÁ EM WIP */
-	scanf(" %d", &tam);
+	printf("Primeiramente, escolha o tamanho que sera utilizado na criacao do labirinto: ");
+	scanf(" %d", &tam); // recebe o tamanho desejado
 	LAB_CriarLabirinto(&labTeste, tam); // criação do labirinto
-	while ((getchar()) != '\n'); //limpa o buffer do scanf
+	while ((getchar()) != '\n'); // limpa o buffer do scanf
 
 	printf("\n\n======================= MANIPULACAO DO LABIRINTO =======================\n");
 	printf("\nOS SEGUINTES COMANDOS ESTAO DISPONIBILIZADOS PARA MANIPULACAO DO LABIRINTO: \n\n");
 
-	printf("->'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO APOS TER APERTADO A TECLA 'ENTER' OU TER DADO UM ESPACO ENTRE AS LETRAS:\n\n%c'p', para inserir uma parede\n%c'v', para uma casa vazia\n%c'e', para setar a entrada da matriz\n%c's', para setar a saida da matriz\n\n\n->'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO\n\n%c'n', para andar na direcao norte\n%c'l', para andar na direcao leste\n%c's', para andar na direcao sul\n%c'o', para andar na direcao oeste\n\n->'d' PARA DESTRUIR O LABIRINTO\n->'m' PARA MOSTRAR O LABIRINTO\n->'r' PARA RESOLVER O LABIRINTO\n->'t' PARA REVER O TUTORIAL\n->'q' PARA FINALIZAR O PROGRAMA\n\n", marcador, marcador, marcador, marcador, marcador, marcador, marcador);
+	printf("->'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO APOS TER APERTADO A TECLA 'ENTER' OU TER DADO UM ESPACO ENTRE AS LETRAS:\n%c'p', para inserir uma parede\n%c'v', para uma casa vazia\n%c'e', para setar a entrada da matriz\n%c's', para setar a saida da matriz\n\n\n->'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO, SEGUIDO DO SEU PARAMETRO APOS TER APERTADO A TECLA 'ENTER' OU TER DADO UM ESPACO ENTRE AS LETRAS:\n%c'n', para andar na direcao norte\n%c'l', para andar na direcao leste\n%c's', para andar na direcao sul\n%c'o', para andar na direcao oeste\n\n->'d' PARA DESTRUIR O LABIRINTO\n->'m' PARA MOSTRAR O LABIRINTO\n->'r' PARA RESOLVER O LABIRINTO\n->'t' PARA REVER O TUTORIAL\n->'q' PARA FINALIZAR O PROGRAMA\n\n", 250, 250, 250, 250, 250, 250, 250);
 		
-	while(comando != 'q') { 
+	while(comando != 'q') { // while q so termina qnd o usuario digitar 'q'
 		printf("Digite o comando desejado seguido do parametro, caso necessario: ");
-		scanf("%c", &comando);
+		scanf(" %c", &comando); // recebe o comando desejado
 
-		if (comando >= 65 && comando <= 90) //transformar letras maiusculas em minusculas
+		if (comando >= 65 && comando <= 90) // transforma letras maiusculas em minusculas
 			comando += 32;
-		//as funcoes ainda precisam receber os condrets para saber se tudo funcionou corretamente
-		if (comando == 'i') { //inserir elemento
-			scanf(" %c", &parametro);
-			
-			if (parametro >= 65 && parametro <= 90)
+
+		if (comando == 'i') { // inserir elemento
+			scanf(" %c", &parametro); // recebe o parametro que ja foi dado junto ao comando
+
+			if (parametro >= 65 && parametro <= 90) // transforma letras maiusculas em minusculas
 				parametro += 32;
-			if (parametro == 'p' || parametro == 'v' || parametro == 'e' || parametro == 's'){
+
+			if (parametro == 'p' || parametro == 'v' || parametro == 'e' || parametro == 's'){ // determina se o parametro e valido
 				if(parametro == 'p')
 					parametroENUM = LAB_ElemParede;
 				else if(parametro == 'v')
@@ -44,17 +69,25 @@ int main(void) {
 					parametroENUM = LAB_ElemEntrada;
 				else if(parametro == 's')
 					parametroENUM = LAB_ElemSaida;
-				LAB_InserirElemento( labTeste, parametroENUM );
-				printf("Elemento inserido com sucesso!\n");
+				CondRetObtido = LAB_InserirElemento( labTeste, parametroENUM );
+
+				if (CondRetObtido == LAB_CondRetLabirintoNaoExiste)
+					printf("Ocorreu um problema durante a criacao do labirinto, pois o labirinto nao existe!\n");
+				else if (CondRetObtido == LAB_CondRetElementoInvalido)
+					printf("Nao se pode inserir elementos no lugar da Saida ou da Entrada!\n");
+				else if (CondRetObtido == LAB_CondRetOK)
+					printf("Elemento inserido com sucesso!\n");
 			}
 			else
 				printf("Parametro invalido!\n");
 		}
 
-		else if (comando == 'a'){ //assumindo como placeholder q andou com sucesso, msm q isso n seja verdade
+		else if (comando == 'a'){ // andar para uma direcao
 			scanf(" %c", &parametro);
+
 			if (parametro >= 65 && parametro <= 90)
 				parametro += 32;
+
 			if (parametro == 'n' || parametro == 'l' || parametro == 's' || parametro == 'o'){
 				if(parametro == 'n')
 					dir=LAB_DirNorte;
@@ -64,31 +97,60 @@ int main(void) {
 					dir=LAB_DirSul;
 				else if(parametro == 'o')
 					dir=LAB_DirOeste;
-				LAB_AndarDirecao( labTeste, dir );
-				printf("Andou com sucesso! Agora na coordenada: (x,y)\n");
+				CondRetObtido = LAB_AndarDirecao( labTeste, dir );
+
+				if (CondRetObtido == LAB_CondRetLabirintoNaoExiste)
+					printf("Ocorreu um problema durante a criacao do labirinto, pois o labirinto nao existe!\n");
+				else if (CondRetObtido == LAB_CondRetDirecaoNaoExisteOuInvalida)
+					printf("Nao se pode andar para fora do labirinto!\n");
+				else if (CondRetObtido == LAB_CondRetOK){
+					LAB_ReceberCoordenadas( labTeste, &x, &y );
+					printf("Andou com sucesso! Agora na coordenada: (%d,%d)\n", x, y);
+				}
 			}
 			else
 				printf("Parametro invalido!\n");
 		}
 
-		else if (comando == 'd'){ //Deixando aqui ja pronto, mas se vc tem uma funcao de destruir labirinto, o usuario tem q ter uma opcao para criar um novo labirinto, pq apos destruir o programa se tornaria inutil
-			LAB_DestruirLabirinto( &labTeste );
+		else if (comando == 'd'){ // destroi funcao
+			CondRetObtido = LAB_DestruirLabirinto( &labTeste );
 			printf("Labirinto destruido com sucesso!\n");
+			printf("Agora que o labirinto foi destruido, voce pode escolher entre finalizar o programa, digitando 'q', ou recriar o labirinto, digitando 'c'!\n");
+			
+			while(comando != 'q' && comando != 'c'){
+				printf("Digite o comando desejado: ");
+				scanf(" %c", &comando);
+				if (comando == 'c'){
+					printf("Escolha o tamanho do labirinto: ");
+					scanf(" %d", &tam);
+					LAB_CriarLabirinto(&labTeste, tam);
+				}
+				else if(comando != 'q')
+					printf("COMANDO INVALIDO!\n");
+			}
 		}
 
-		else if (comando == 'm')
-			LAB_ImprimirLabirinto( labTeste );
+		else if (comando == 'm'){ // imprimir labirinto
+			CondRetObtido = LAB_ImprimirLabirinto( labTeste );
 
-		else if (comando == 'r')
-			LAB_ExibeSolucao( labTeste ); //N sei se essa funcao vai ser a q acha a solucao, mas e a mais proxima do q a gnt tem, entao botei como placeholder
+			if (CondRetObtido == LAB_CondRetLabirintoNaoExiste)
+				printf("Ocorreu um problema durante a criacao do labirinto, pois o labirinto nao existe!\n");
+		}
 
-		else if (comando == 't')
-			printf("\n'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO:\n-'p', para inserir uma parede\n-'v', para uma casa vazia\n-'e', para setar a entrada da matriz\n-'s', para setar a saida da matriz\n\n'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO\n-'n', para andar na direcao norte\n-'l', para andar na direcao leste\n-'s', para andar na direcao sul\n-'o', para andar na direcao oeste\n\n'd' PARA DESTRUIR O LABIRINTO\n'm' PARA MOSTRAR O LABIRINTO\n'r' PARA RESOLVER O LABIRINTO\n't' PARA REVER O TUTORIAL\n'q' PARA FINALIZAR O PROGRAMA\n\n", marcador, marcador, marcador, marcador, marcador, marcador, marcador, marcador);
+		else if (comando == 'r'){ // resolver labirinto
+			CondRetObtido = LAB_ExibeSolucao( labTeste );
 
-		else if (comando != 'q')
+			if (CondRetObtido == LAB_CondRetLabirintoNaoExiste)
+				printf("Ocorreu um problema durante a criacao do labirinto, pois o labirinto nao existe!\n");
+		}
+
+		else if (comando == 't') // imprimir o tutorial
+			printf("\n'i', PARA INSERIR UM ELEMENTO NA CASA CORRENTE DO LABIRINTO, SEGUIDO DO SEU PARAMETRO:\n-'p', para inserir uma parede\n-'v', para uma casa vazia\n-'e', para setar a entrada da matriz\n-'s', para setar a saida da matriz\n\n'a' PARA ANDAR PARA A PROXIMA CASA DO LABIRINTO\n-'n', para andar na direcao norte\n-'l', para andar na direcao leste\n-'s', para andar na direcao sul\n-'o', para andar na direcao oeste\n\n'd' PARA DESTRUIR O LABIRINTO\n'm' PARA MOSTRAR O LABIRINTO\n'r' PARA RESOLVER O LABIRINTO\n't' PARA REVER O TUTORIAL\n'q' PARA FINALIZAR O PROGRAMA\n\n", 250, 250, 250, 250, 250, 250, 250, 250);
+
+		else if (comando != 'q') // fechar o programa
 			printf("COMANDO INVALIDO!\n");
 
-		while ((getchar()) != '\n'); //limpa o buffer do scanf
+		while ((getchar()) != '\n'); // limpa o buffer do scanf
 	}
 
 	return 0;
